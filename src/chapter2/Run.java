9e1a8498087f9af2f16bd7cf5c0de4b5da478015
xzyuan <xzyuan@mail.ustc.edu.cn>
2018-06-29 22:14:24 +0800

    second commit

---
+++ src/chapter2/Run.java
@@ -0,0 +1,23 @@
+package chapter2;
+
+//两个线程，互相等待取得对方的锁
+public class Run {
+    public static void main(String[] args) {
+        try {
+            DealThread t1 = new DealThread();
+            t1.setFlag("a");
+
+            Thread thread1 = new Thread(t1);
+            thread1.start();
+
+            Thread.sleep(100);
+            t1.setFlag("b");]
+
+            Thread thread2 = new Thread(t1);
+            thread2.start();
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
